72ea82d74942ed28ee0cbc229e21d270ffd38105
Luis Rogerio Lima dos Santos <luiz.rogerio.lima@hotmail.com>
2021-03-16 20:14:20 -0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,17 @@
+//Comandos NPM
+//npm install --global gulp-cli //Install the gulp command line utility
+//npm install gulp browser-sync gulp sass --save-dev //inatalação dos recursos para rodar o script
+//npm install bootstrap jquery popper.js --save
+//Estruturas de pasta
+//gulpfile.js
+//src
+//  ->assets
+//  ->js
+//  ->css
+//  ->scss
+//Comando para executar o gulp: gulp
+//
+
 var gulp = require('gulp');
 var browsersync = require('browser-sync').create();
 var sass = require('gulp-sass');
